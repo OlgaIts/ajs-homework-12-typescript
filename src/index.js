61817e03f6caa98b9cd9190ef5a043f1b2d7551e
Olga Its <olga-tissen@tutanota.com>
2024-03-19 09:01:29 +0100

    добавила конфиг для Eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Cart from './js/Cart';
 import Movie from './js/Movie';
 import Book from './js/Book';
@@ -5,6 +6,7 @@ import Book from './js/Book';
 // const cart = new Cart()
 
 // cart.add(new Book(1, 'Сказка о царе Салтане', 'А.С. Пушкин', 1525, 48))
-// cart.add(new Movie(2, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключение'], '137 мин. / 2:17', 1234))
+// cart.add(new Movie(2, 'Мстители', 'The Avengers', 2012, 'США', 'Avengers Assemble!',
+// ['фантастика', 'боевик', 'фэнтези', 'приключение'], '137 мин. / 2:17', 1234))
 
 // console.log(cart.items)
